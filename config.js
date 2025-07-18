e1f5e4d4eb432ab66e8acf7e2f08e84674023882
Ninh-CHan <ohzay125@gmail.com>
2025-07-18 09:30:25 +0700

    up

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Xin chào bé Chun của anh",
+    introTitle: 'Chun',
+    introDesc: `Con tằm đắm đuối vì tơ
+    Tôi xay vì rượu ngẩn nhơ vì tình
+    Có duyên ta mới có tình
+    Chào nhau một cái cho mình thêm duyên`,
+    btnIntro: '^^ Hệ hệ hệ ^^',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích lắm <33',
+    btnNo: 'Không nha :3',
+    question: 'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Tối nay 7h, Anh qua đón đi uống rượu nha nha.',
+    btnAccept: 'Okiiiii thì ib anh nha <3. Mãi yêu bé Chun <3',
+    messLink: 'https://www.facebook.com/ninh.tran.103859/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
